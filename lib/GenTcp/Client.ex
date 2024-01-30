8c5744c2662306013520326c7ab43d78addda91e
Samuel Owen <SamHirael@gmail.com>
2024-01-30 19:22:30 +0000

    Hack together a tcp client, no idea if it works!

--- lib/GenTcp/Client.ex
+++ lib/GenTcp/Client.ex
@@ -11,4 +11,29 @@ defmodule GenTcp.Client do
     init_arg = nil
     {:ok, init_arg}
   end
+
+  def handle_call({:send_packet, message, url, port}, _from, state) do
+    Logger.log(:debug, "Attempting to ")
+    {:ok, socket} = :gen_tcp.connect(url, port: port)
+    :ok = send_packet(socket, message)
+    {:ok, data} = read_socket(socket)
+
+    {:reply, data, state}
+  end
+
+  defp send_packet(socket, packet) do
+    :gen_tcp.send(socket, packet)
+  end
+
+  defp read_socket(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    {:ok, data}
+  end
+
+  # End 'Private' interfaces
+
+  # Public Interfaces
+  def send_message(message, url, port) do
+    GenServer.call(__MODULE__, {:send_packet, message, url, port})
+  end
 end
