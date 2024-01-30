02b8e3858b782d982da13d49b3d463fee5539b6b
Samuel Owen <SamHirael@gmail.com>
2024-01-30 18:32:41 +0000

    Add start_link function to GenTcp.Application
    This commit adds a `start_link` function to the `GenTcp.Application` module. The function starts the `GenTcp.System` process, which in turn starts the `GenTcp.Server` and `GenTcp.Client` processes. This change enables the application to handle both server and client functionality.

--- lib/GenTcp/Application.ex
+++ lib/GenTcp/Application.ex
@@ -1,4 +1,7 @@
 defmodule GenTcp.Application do
-  
+  use Application
 
+  def start(_, _) do
+    GenTcp.System.start_link()
+  end
 end
--- lib/GenTcp/System.ex
+++ lib/GenTcp/System.ex
@@ -1,14 +1,11 @@
 defmodule GenTcp.System do
-  use Supervisor
-
-  def start_link(_) do
-    Supervisor.start_link(__MODULE__, nil, name: __MODULE__)
-  end
-
-  @impl true
-  def init(_) do
-    children = [
-      {GenTcp.Server, nil}
-    ]
+  def start_link() do
+    Supervisor.start_link(
+      [
+        {GenTcp.Server, nil},
+        {GenTcp.Client, nil}
+      ],
+      strategy: :one_for_one
+    )
   end
 end
