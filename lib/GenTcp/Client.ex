0b614becc8b69127de0dca3326e1c6ebdcae3786
Samuel Owen <SamHirael@gmail.com>
2024-01-30 18:39:10 +0000

    Refactor GenTcp.Client start_link function

--- lib/GenTcp/Client.ex
+++ lib/GenTcp/Client.ex
@@ -1,8 +1,12 @@
 defmodule GenTcp.Client do
   use GenServer
 
-
   def start_link(_) do
-    GenServer.start(__MODULE__)
+    GenServer.start(__MODULE__, nil)
+  end
+
+  def init(_) do
+    init_arg = nil
+    {:ok, init_arg}
   end
 end
