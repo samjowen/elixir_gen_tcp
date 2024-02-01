ff33887be42376e9f16ee7e9a1bfa0580d64d660
Samuel Owen <SamHirael@gmail.com>
2024-02-01 22:31:47 +0000

    Refactor GenTcp.Client module to improve error handling and concurrency

--- lib/GenTcp/Client.ex
+++ lib/GenTcp/Client.ex
@@ -2,31 +2,37 @@ defmodule GenTcp.Client do
   require Logger
   use GenServer
 
-  def start_link(_) do
-    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
-  end
-
-  def init(_) do
+  @impl true
+  def init({hostname, port}) do
     Logger.log(:debug, "#{__MODULE__}: Starting Client.")
-    init_arg = nil
-    {:ok, init_arg}
-  end
-
-  def handle_call({:send_packet, message, hostname, port}, _from, state) do
-    Logger.log(:debug, "#{__MODULE__}: Attempting to send packet.")
-    # Be careful with packet: :line, it will not send the packet until a newline is sent
     opts = [:binary, :inet, active: false, packet: :raw]
 
-    {:ok, socket} =
-      :gen_tcp.connect(hostname, port, opts)
+    case :gen_tcp.connect(hostname, port, opts) do
+      {:ok, socket} ->
+        {:ok, socket}
 
-    Logger.log(:debug, "#{__MODULE__}: Socket made: #{inspect(socket)}")
-    :ok = send_packet(socket, message)
-    {:ok, data} = read_socket(socket)
-    Logger.log(:debug, "#{__MODULE__}: Data: #{inspect(data)}")
-    {:reply, data, state}
+      {:error, reason} ->
+        Logger.error(reason)
+        {:error, reason}
+    end
   end
 
+  @impl true
+  def handle_call({:send_packet, message}, _from, socket) do
+    :ok = send_packet(socket, message)
+    {:ok, data} = read_socket(socket)
+    {:reply, data, socket}
+  end
+
+  @impl true
+  def handle_cast({:send_packets, message, amount}, socket) do
+    Enum.each(0..amount, fn _ -> Task.start(fn -> send_packet(socket, message) end) end)
+    {:noreply, socket}
+  end
+
+  # End 'Private' interfaces
+
+  # Private Helpers
   defp send_packet(socket, packet) do
     Logger.log(:debug, "#{__MODULE__}: Sending packet to server...")
     :ok = :gen_tcp.send(socket, packet)
@@ -39,10 +45,20 @@ defmodule GenTcp.Client do
     {:ok, data}
   end
 
-  # End 'Private' interfaces
-
   # Public Interfaces
-  def send_message(message) do
+
+  @spec start(binary(), integer()) :: :ignore | {:error, any()} | {:ok, pid()}
+  def start(hostname, port) when is_binary(hostname) and is_integer(port) do
+    GenServer.start(__MODULE__, {hostname, port}, name: __MODULE__)
+  end
+
+  @spec send_message(binary()) :: any()
+  def send_message(message) when is_binary(message) do
     GenServer.call(__MODULE__, {:send_packet, message, :localhost, 4000})
+  end
+
+  @spec send_messages(binary(), integer()) :: any()
+  def send_messages(message, amount) when is_binary(message) and is_integer(amount) do
+    GenServer.call(__MODULE__, {:send_packets, message, amount})
   end
 end
