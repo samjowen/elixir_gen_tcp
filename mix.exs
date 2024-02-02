60f8056f14a8041bffead2d35e6f73e259459c3b
Samuel Owen <SamHirael@gmail.com>
2024-02-02 23:47:15 +0000

    Bump version to 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElixirGenTcp.MixProject do
   def project do
     [
       app: :elixir_gen_tcp,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
