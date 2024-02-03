1cee3911a34ab8596383d34eea24a398fc8d23f6
Samuel Owen <SamHirael@gmail.com>
2024-02-03 00:08:44 +0000

    Update Dockerfile and mix.exs versions

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 # syntax=docker/dockerfile:1
 
-FROM elixir:1.15-alpine
+FROM hexpm/elixir:1.16.1-erlang-26.2.1-alpine-3.17.5
 COPY ./ /app/
 WORKDIR /app
 RUN mix deps.get
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ElixirGenTcp.MixProject do
     [
       app: :elixir_gen_tcp,
       version: "0.1.4",
-      elixir: "~> 1.15",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
